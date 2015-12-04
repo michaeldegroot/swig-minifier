9cb1eb2a4d63ab464662b9ada45608bab1f8111f
michaeldegroot <specamps@gmail.com>
2015-12-04 15:57:20 +0100

    version 0.1.5

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var swig = require('swig');
 var md5 = require('md5');
 var fs = require('fs');
 var os = require('os');
+var path = require('path');
 var NodeCache = require( "node-cache" );
 var myCache = new NodeCache({ stdTTL: 0, checkperiod: 120 });
 
-var options = {};
+var options;
 
 exports.init = function(sets){
+	var folder = path.join(os.tmpdir(),"swig-minifier");
+	fs.existsSync(folder) || fs.mkdirSync(folder);
+	
 	options = sets;
+}
+
+exports.minify = function(result){
+	return htmlminify(result, {minifyJS: true, minifyCSS: true, removeComments: true, collapseWhitespace: true});
 }
 
 exports.engine = function(pathName, locals, cb) {
@@ -19,55 +27,41 @@ exports.engine = function(pathName, locals, cb) {
 			options.cacheType = "file";
 		}
         var html;
-		var cacheExists = false;
 		var key = md5(result);
 		
 		if(options.cacheType=="memory"){
 			myCache.get(key, function(err, value){
 				if(!err){
-					if(value == undefined){
-						val = false;
-					}else{
-						val = value;
-					}
 					if(val){
-						var cacheExists = true;
 						return cb(err, val);
 					}else{
-						if (!err) {
-							html = htmlminify(result, {
-								minifyJS: true,
-								minifyCSS: true,
-								removeComments: true,
-								collapseWhitespace: true,
-							});
-						}
+						html = exports.minify(result);
 						myCache.set(key, html, 0);
 						return cb(err, html);
 					}
+				}else{
+					throw err;
 				}
 			});
 		}
 		if(options.cacheType=="file"){
-			var tmp = os.tmpdir();
-			var path = tmp + "/"+key+".html";
+			var file = path.join(os.tmpdir(),"swig-minifier",key+".html");
 			
-			fs.stat(path, function(err, stat) {
+			fs.stat(file, function(err, stat) {
 				if(err == null) {
-					var cacheExists = true;
-					fs.readFile(path, function(err,data){
+					fs.readFile(file, function(err,data){
 						if(err) throw err;
 						return cb(err, data.toString('utf8'));
 					});
 				} else if(err.code == 'ENOENT') {
-					html = htmlminify(result, {
-						minifyJS: true,
-						minifyCSS: true,
-						removeComments: true,
-						collapseWhitespace: true,
-					});
-					fs.writeFile(path, html, function(err) {
-						if(err) throw err;
+					html = exports.minify(result);
+					fs.writeFile(file, html, function(err) {
+						if(err){
+							if(err.code == "ENOENT"){
+								throw "ERROR: swig-minifier - Call the .init function before calling the .engine function. Check the documentation at NPM on how to use swig-minifier";
+							}
+							throw err;
+						}
 						return cb(err, html);
 					}); 
 				} else {
